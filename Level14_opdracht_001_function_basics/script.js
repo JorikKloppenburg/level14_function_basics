a481eefea2ab97f3396420e5c773992fcff3f5f3
Jorik Kloppenburg <78646379+JorikKloppenburg@users.noreply.github.com>
2021-06-11 10:11:23 +0200

    Add files via upload

---
+++ Level14_opdracht_001_function_basics/script.js
@@ -0,0 +1,30 @@
+// beschrijven van de function makePizza
+const makePizza = function () {
+    console.log("Verwarm de oven voor op 180 graden")
+    console.log("Haal een ronde vorm uit het deeg")
+    console.log("Besmeer het deeg met tomatensaus en beleg naar keuze")
+    console.log("Doe de pizza in de oven voor 10 minuten")
+};
+
+// beschrijven van de function makeSushi
+const makeSushi = function () {
+    console.log("Kook de rijst volgens de instructies")
+    console.log("Leg het matje klaar en plaats denorivel met rijst erop")
+    console.log("Beleg de rijst met zalm en advocado")
+    console.log("Rol het geheel samen op")
+};
+
+// run the code, are you getting the result you expect? Why (not)?
+// Nog niet, enkel de code is omschreven. Nog niet de GET functie.
+
+makePizza();
+makePizza();
+makePizza();
+makeSushi();
+
+//So now, let's for a minute pretend that we work in a restaurant and we get an order for three meals of sushi and two pizzas.
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
